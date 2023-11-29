c950d43cb7f229b03dcfc7a3ebd57d33a46ddbff
Nandikonda Srikanth <113960390+nsrikanth97@users.noreply.github.com>
2023-11-28 23:23:51 -0500

    Update Views-Final-Project.sql

--- Views-Final-Project.sql
+++ Views-Final-Project.sql
@@ -35,7 +35,6 @@ GO;
 
 --View to fetch the financial summary of all the users to contain 
 --bank balance, credit card due, loan payment due and  investment value 
-
 create view 
 FinancialSummary
 AS
@@ -46,6 +45,40 @@ SELECT
 	dbo.GetInvestmentValue(p.profile_id),
 	dbo.GetPendingLoanPayment(p.profile_id)
 from profile p;
+
+-- View to display All the transactions(both credit and debit) at one place
+CREATE VIEW vw_TransactionOverview AS
+SELECT
+    'Bank' AS TransactionSource,
+    p.FIRST_NAME,
+    BANK_TRANSACTION_ID AS TransactionID,
+    a.ACCOUNT_ID,
+    bi.NAME as Bank_Name,
+    AMOUNT,
+    TRANSACTION_DATE,
+    b.TRANSACTION_TYPE,
+    e.name as Expense_category
+FROM BANK_TRANSACTION b
+left join EXPENSE_CATEGORY e on b.EXPENSE_CATEGORY_ID = e.EXPENSE_CATEGORY_ID
+join ACCOUNTS a on b.ACCOUNT_ID = a.ACCOUNT_ID
+join BANK_INFORMATION bi on bi.BANK_ID = a.BANK_ID
+Join profile p on a.PROFILE_ID = p.PROFILE_ID
+UNION
+SELECT
+    'Credit Card' AS TransactionSource,
+    p.FIRST_NAME,
+    CC_TRANSACTION_ID AS TransactionID,
+    cc.CREDIT_CARD_ID AS ACCOUNT_ID,
+    bi.NAME as Bank_Name,
+    AMOUNT,
+    TRANSACTION_DATE,
+    TRANSACTION_TYPE,
+		e.name as Expense_category
+FROM CREDIT_CARD_TRANSACTION c
+left join EXPENSE_CATEGORY e on c.EXPENSE_CATEGORY_ID = e.EXPENSE_CATEGORY_ID
+join CREDIT_CARD cc on cc.CREDIT_CARD_ID = c.CREDIT_CARD_ID
+join BANK_INFORMATION bi on bi.BANK_ID = cc.BANK_ID
+Join profile p on cc.PROFILE_ID = p.PROFILE_ID;
 
 
 
